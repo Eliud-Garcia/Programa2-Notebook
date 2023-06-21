3d26c27b24848f74c09fc8ca1a6f1bfcb9e26f0d
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-06-20 22:13:57 -0500

    java template

--- Java/Template.java
+++ Java/Template.java
@@ -11,6 +11,8 @@ public class Template {
 
     static void solve() throws IOException {
 
+
+        
     }
 
     public static void main(String[] args) throws IOException {
@@ -118,48 +120,7 @@ public class Template {
         }
     }
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-    }
+    
 
     static class InputReader {
         private InputStream stream;
@@ -318,103 +279,5 @@ public class Template {
         }
     }
 
-    static class SegmentTree<T> {
-        int n;
-        ArrayList<T> st;
-        T neutro;
-        BinaryOperator<T> oper;
-        TreeMap<T, Integer> freq;
-
-        SegmentTree(ArrayList<T> a, BinaryOperator<T> op, T neutro) {
-            n = a.size();
-            st = new ArrayList<>(Collections.nCopies(n * 4, null));
-            this.neutro = neutro;
-            oper = op;
-            freq = new TreeMap<>();
-            build(1, 0, n - 1, a);
-        }
-
-        void build(int v, int tl, int tr, ArrayList<T> a) {
-            if (tl == tr) {
-                st.set(v, a.get(tl));
-                freq.put(a.get(tl), freq.getOrDefault(a.get(tl), 0) + 1);
-                return;
-            }
-            int tm = (tr + tl) / 2;
-            build(v * 2, tl, tm, a);
-            build(v * 2 + 1, tm + 1, tr, a);
-
-            st.set(v, oper.apply(st.get(v * 2), st.get(v * 2 + 1)));
-
-        }
-
-        T query(int v, int tl, int tr, int l, int r) {
-            if (tl > r || tr < l)
-                return neutro;
-            if (l <= tl && tr <= r) {
-                return st.get(v);
-            }
-            int tm = (tl + tr) / 2;
-
-            return oper.apply(query(v * 2, tl, tm, l, r), query(v * 2 + 1, tm + 1, tr, l, r));
-        }
-
-        void upd(int v, int tl, int tr, int pos, T val) {
-
-            if (tl == tr) {
-                if (freq.containsKey(st.get(v))) {
-                    freq.put(st.get(v), freq.get(st.get(v)) - 1);
-                }
-                st.set(v, val);
-                freq.put(val, freq.getOrDefault(val, 0) + 1);
-                return;
-            }
-            int tm = (tr + tl) / 2;
-            if (pos <= tm)
-                upd(v * 2, tl, tm, pos, val);
-            else
-                upd(v * 2 + 1, tm + 1, tr, pos, val);
-            st.set(v, oper.apply(st.get(v * 2), st.get(v * 2 + 1)));
-
-        }
-
-        void upd(int pos, T val) {
-            upd(1, 0, n - 1, pos, val);
-        }
-
-        T query(int l, int r) {
-            return query(1, 0, n - 1, l, r);
-        }
-
-        int getMinFrequency(int l, int r) {
-            T min = query(l, r);
-            return freq.getOrDefault(min, 0);
-        }
-    }
-
-    /*
-     * Example
-     * 
-     * inicializar
-     * 
-     * ArrayList<Long> arr = new ArrayList<>();
-     * BinaryOperator<Long> f = (a, b) -> Math.min(a, b);
-     * Long neutro = Long.valueOf(1000000007);
-     * SegmentTree<Long> st = new SegmentTree<Long>(arr, f, neutro);
-     * 
-     * 
-     * 
-     * while (m-- > 0) {
-     * int o = en.nextInt(), l = en.nextInt(), r = en.nextInt();
-     * if (o == 1) {
-     * Long aux = (long) r;
-     * st.upd(l, aux);
-     * } else {
-     * r--;
-     * Long qu = st.query(l, r);
-     * sa.println(qu == null ? 0 : qu);
-     * }
-     * }
-     */
-
+   
 }
