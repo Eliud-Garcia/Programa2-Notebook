de314ab634cffdb6090422bde062d8b68950df4f
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-07-16 15:36:07 -0500

    changes

---
+++ Java/Bitwise/Generate all subsets.java
@@ -0,0 +1,39 @@
+   
+
+//  O(n · 2^n )
+
+    static List<List<Integer>> generateSubsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        int n = nums.length;
+        // Total number of subsets = 2^n
+
+        // Generate all subsets
+        for (int i = 0; i < 1 << n; i++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            subsets.add(subset);
+        }
+        return subsets;
+    }
+
+    static void solve() throws IOException {
+        int arr[] = { 1, 2, 3 };
+        List<List<Integer>> x = generateSubsets(arr);
+        for (List<Integer> i : x) {
+            sa.println(i);
+        }
+        /*
+         * []
+         * [1]
+         * [2]
+         * [1, 2]
+         * [3]
+         * [1, 3]
+         * [2, 3]
+         * [1, 2, 3]
+         */
+    }
