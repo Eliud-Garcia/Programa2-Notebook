176de8c3399c14f5d1b816bc333e5a5bcd0e9626
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-07-16 14:56:39 -0500

    pair class

---
+++ Java/Math/generatePermutation.java
@@ -0,0 +1,29 @@
+// O(n!) 
+
+static void generatePermutation(ArrayList<Integer> arr, int n) {
+        if (n == 1) {
+            printArr(arr);
+        } else {
+            for (int i = 0; i < n; i++) {
+                generatePermutation(arr, n - 1);
+                if (n % 2 == 0) {
+                    swap(arr, i, n - 1);
+                } else {
+                    swap(arr, 0, n - 1);
+                }
+            }
+        }
+    }
+
+    static void swap(ArrayList<Integer> arr, int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+
+    static void printArr(ArrayList<Integer> arr) {
+        for (int num : arr) {
+            sa.print(num + " ");
+        }
+        sa.println();
+    }
---
+++ utils/pair.java
@@ -0,0 +1,34 @@
+static class pair<F extends Comparable<F>, S extends Comparable<S>> implements Comparable<pair<F, S>> {
+        F first;
+        S second;
+
+        public pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        /*
+         * • If object x is less than object y, return a negative number
+         * • If object x is greater than object y, return a positive number
+         * • If object x is equal to object y, return 0
+         */
+        @Override
+        public int compareTo(pair<F, S> other) {
+            // decresing order = return -first.compareTo(other.first);
+            if (first == other.first) {
+                return second.compareTo(other.second);
+            } else {
+                return first.compareTo(other.first);
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * first.hashCode() + second.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "(" + first + ", " + second + ")";
+        }
+    }
