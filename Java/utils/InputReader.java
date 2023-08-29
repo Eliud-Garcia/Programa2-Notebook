9723736430ff991fe97d6ecba9ab1e1d0adcec8e
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-08-29 14:44:08 -0500

    IO

--- Java/utils/InputReader.java
+++ Java/utils/InputReader.java
@@ -3,13 +3,14 @@ Mas rapido que el BufferedReader
     // en = new InputReader("input.txt");
     // sa = new BufferedWriter(new BufferedWriter(new FileWriter("output.txt")));
 
-    static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private SpaceCharFilter filter;
-        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ static class InputReader {
+
+        InputStream stream;
+        byte[] buf = new byte[1024];
+        int curChar;
+        int numChars;
+        SpaceCharFilter filter;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         public InputReader() {
             this.stream = System.in;
@@ -19,9 +20,10 @@ Mas rapido que el BufferedReader
             this.stream = new FileInputStream(file);
         }
 
-        public int read() {
-            if (numChars == -1)
+        int read() {
+            if (numChars == -1) {
                 throw new InputMismatchException();
+            }
             if (curChar >= numChars) {
                 curChar = 0;
                 try {
@@ -30,13 +32,14 @@ Mas rapido que el BufferedReader
                     throw new InputMismatchException();
                 }
 
-                if (numChars <= 0)
+                if (numChars <= 0) {
                     return -1;
+                }
             }
             return buf[curChar++];
         }
 
-        public String nextLine() {
+        String nextLine() {
             String str = "";
             try {
                 str = br.readLine();
@@ -46,10 +49,11 @@ Mas rapido que el BufferedReader
             return str;
         }
 
-        public int nextInt() {
+        int nextInt() {
             int c = read();
-            while (isSpaceChar(c))
+            while (isSpaceChar(c)) {
                 c = read();
+            }
             int sgn = 1;
             if (c == '-') {
                 sgn = -1;
@@ -57,8 +61,9 @@ Mas rapido que el BufferedReader
             }
             int res = 0;
             do {
-                if (c < '0' || c > '9')
+                if (c < '0' || c > '9') {
                     throw new InputMismatchException();
+                }
                 res *= 10;
                 res += c - '0';
                 c = read();
@@ -66,10 +71,11 @@ Mas rapido que el BufferedReader
             return res * sgn;
         }
 
-        public long nextLong() {
+        long nextLong() {
             int c = read();
-            while (isSpaceChar(c))
+            while (isSpaceChar(c)) {
                 c = read();
+            }
             int sgn = 1;
             if (c == '-') {
                 sgn = -1;
@@ -77,8 +83,9 @@ Mas rapido que el BufferedReader
             }
             long res = 0;
             do {
-                if (c < '0' || c > '9')
+                if (c < '0' || c > '9') {
                     throw new InputMismatchException();
+                }
                 res *= 10;
                 res += c - '0';
                 c = read();
@@ -86,10 +93,11 @@ Mas rapido que el BufferedReader
             return res * sgn;
         }
 
-        public double nextDouble() {
+        double nextDouble() {
             int c = read();
-            while (isSpaceChar(c))
+            while (isSpaceChar(c)) {
                 c = read();
+            }
             int sgn = 1;
             if (c == '-') {
                 sgn = -1;
@@ -97,10 +105,12 @@ Mas rapido que el BufferedReader
             }
             double res = 0;
             while (!isSpaceChar(c) && c != '.') {
-                if (c == 'e' || c == 'E')
+                if (c == 'e' || c == 'E') {
                     return res * Math.pow(10, nextInt());
-                if (c < '0' || c > '9')
+                }
+                if (c < '0' || c > '9') {
                     throw new InputMismatchException();
+                }
                 res *= 10;
                 res += c - '0';
                 c = read();
@@ -109,10 +119,12 @@ Mas rapido que el BufferedReader
                 c = read();
                 double m = 1;
                 while (!isSpaceChar(c)) {
-                    if (c == 'e' || c == 'E')
+                    if (c == 'e' || c == 'E') {
                         return res * Math.pow(10, nextInt());
-                    if (c < '0' || c > '9')
+                    }
+                    if (c < '0' || c > '9') {
                         throw new InputMismatchException();
+                    }
                     m /= 10;
                     res += (c - '0') * m;
                     c = read();
@@ -121,10 +133,11 @@ Mas rapido que el BufferedReader
             return res * sgn;
         }
 
-        public String readString() {
+        String next() {
             int c = read();
-            while (isSpaceChar(c))
+            while (isSpaceChar(c)) {
                 c = read();
+            }
             StringBuilder res = new StringBuilder();
             do {
                 res.appendCodePoint(c);
@@ -133,17 +146,14 @@ Mas rapido que el BufferedReader
             return res.toString();
         }
 
-        public boolean isSpaceChar(int c) {
-            if (filter != null)
+        boolean isSpaceChar(int c) {
+            if (filter != null) {
                 return filter.isSpaceChar(c);
+            }
             return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
         }
 
-        public String next() {
-            return readString();
-        }
-
-        public interface SpaceCharFilter {
+        interface SpaceCharFilter {
             public boolean isSpaceChar(int ch);
         }
 
