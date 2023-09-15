26801f8a2b4f4e9459b6e2ff2afbc8b18fe3b8cc
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-09-15 17:42:16 -0500

    cpp

---
+++ cpp/template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define ll long long
+#define forn(i, n){} for(int i = 0; i< n; i++){}
+#define fora(i, a, b) for(int i=a; i<b; i++);
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    
+    return 0;
+}
