794a1fb779211ab5c16d0c1fadc8796b6d7fef6f
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-09-22 14:51:22 -0500

    dijkstra

--- Java/Graph/Dijkstra.java
+++ Java/Graph/Dijkstra.java
@@ -1,107 +1,93 @@
-import java.util.*;
 
 
-
-
-public class Dijkstra {
-
-    static class Pair {
-        int first;
-        int second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + first + " , " + second + ")";
-        }
-    }
-
-    static final int MAXN = 100003;
     static final int INF = Integer.MAX_VALUE;
-
-    static List<List<Pair>> adjList;
-    static int n;
-    static int[] dist = new int[MAXN];
-    static int[] par = new int[MAXN];
-    static BitSet isDone = new BitSet(MAXN);
+    static List<int[]> adj[];
+    static int[] dist;
+    static int[] par;
+    static BitSet isDone;
 
     // dijkstra(int source, int des)
     static boolean dijkstra(int s, int t) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(p -> p.first));
+
+        // new int[] {destino, peso};
+    
+        Comparator<int[]> cmp = new Comparator<int[]>() {
+            @Override
+            public int compare(int a[], int b[]) {
+                return Integer.compare(a[0], b[0]);
+            }
+        };
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
+
         Arrays.fill(dist, INF);
 
-        pq.add(new Pair(0, s));
+        pq.add(new int[] { 0, s });
+
         dist[s] = 0;
         par[s] = -1;
 
         while (!pq.isEmpty()) {
-            int u = pq.poll().second;
+            int u = pq.poll()[1];
 
-            if (u == t)
-                return true;
+            if (u == t) return true;
 
             isDone.set(u, true);
 
-            for (Pair pr : adjList.get(u)) {
-                int v = pr.first;
-                int w = pr.second;
+            for (int[] next : adj[u]) {
+                int v = next[0];
+                int w = next[1];
 
                 if (!isDone.get(v) && dist[u] + w < dist[v]) {
                     dist[v] = dist[u] + w;
-                    pq.add(new Pair(dist[v], v));
+                    pq.add(new int[] { dist[v], v });
                     par[v] = u;
                 }
             }
         }
-
         return false;
     }
 
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int m = nextInt();
 
-    /*      Dijkstra
-     *      shortest path undirected graph
-     *      https://codeforces.com/contest/20/problem/C
-     */
+        adj = new ArrayList[n + 3];
+        dist = new int[n + 3];
+        par = new int[n + 3];
+        isDone = new BitSet(n + 3);
 
-    static void solve() throws IOException {
-        n = en.nextInt();
-        int m = en.nextInt();
 
-        adjList = new ArrayList<>(n + 3);
-        for (int i = 0; i < n + 3; i++) {
-            adjList.add(new ArrayList<>());
+        for (int i = 0; i < n + 3; i++){
+            adj[i] = new ArrayList<>();
         }
 
-        dist = new int[MAXN];
-        par = new int[MAXN];
-        isDone = new BitSet(MAXN);
-
-        int u, v, w;
+        int a, b, w;
         for (int i = 0; i < m; i++) {
-            u = en.nextInt();
-            v = en.nextInt();
-            w = en.nextInt();
-            adjList.get(u).add(new Pair(v, w));
-            adjList.get(v).add(new Pair(u, w));
+            a = nextInt();
+            b = nextInt();
+            w = nextInt();
+
+            //adj[source].add(new int[] {dest, weight});
+            adj[a].add(new int[] { b, w });
+            adj[b].add(new int[] { a, w });
         }
-        //path is found
+
+        // path is found
         if (dijkstra(1, n)) {
             StringBuilder ans = new StringBuilder();
             List<Integer> path = new ArrayList<>();
-            for (v = n; v != -1; v = par[v]) {
-                path.add(v);
-            }
-            //reversing path
-            for (int i = path.size()-1; i >=0; i--) {
-                ans.append(path.get(i)).append(" ");
+
+            for (b = n; b != -1; b = par[b]) path.add(b);
+
+            // reversing path
+            for (int i = path.size() - 1; i >= 0; i--) {
+                ans.append(path.get(i) + " ");
             }
             sa.println(ans);
         } else {
             sa.println("-1");
         }
+        sa.close();
     }
-}
+
