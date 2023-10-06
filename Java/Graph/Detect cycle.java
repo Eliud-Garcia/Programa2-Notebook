32fa5cf315fc0c8a1af6cf6c16515a14beeb5da3
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-10-05 19:25:48 -0500

    cycle

--- Java/Graph/Detect cycle.java
+++ Java/Graph/Detect cycle.java
@@ -1,13 +1,11 @@
 
 
+    // undirected graph
     static List<Integer> graph[];
     static boolean vis[];
     static int parent[];
-    static int dis[];
-    static int ans[];
     static int n, m;
-    static int star_cycle, end_cycle;
-    static boolean found;
+    static int star_cycle = -1, end_cycle = -1;
 
 
     static boolean dfs(int node, int par){
@@ -47,33 +45,92 @@
             graph[b].add(a);
         }
 
-        boolean visitAll = false;
         for (int i = 1; i < n + 1; i++) {
             if(!vis[i] && dfs(i, -1)){
-                visitAll = true;
                 break;
             }
         }
 
-        if(!visitAll){
-            //No hay ciclo
-            sa.println("IMPOSSIBLE");
+        if(star_cycle == -1){
+            sa.println("No hay ciclo");
         }else{
-            //si hay ciclo
+            sa.println("si hay ciclo");
             ArrayList<Integer> path = new ArrayList<>();
             path.add(end_cycle);
             for (int i = end_cycle; i != star_cycle; i=parent[i]) {
                 path.add(parent[i]);
             }
             path.add(end_cycle);
-
-            //sa.println("inicia en "+end_cycle+" termina en "+star_cycle);
             
             sa.println(path.size());
             for (int k : path) {
                 sa.print(k+" ");
             }
 
+        }
+
+        sa.close();
+    }
+
+
+    //########################################################################################
+
+    // directed graph
+
+    
+    static List<Integer> graph[];
+    static int parent[];
+    static int color[];
+    static int n, m;
+    static int cycle_start = -1, cycle_end = -1;
+
+    static boolean dfs(int node) {
+        color[node] = 1;
+        for (int next : graph[node]) {
+            if (color[next] == 0) {
+                parent[next] = node;
+                if (dfs(next)) {
+                    return true;
+                }
+            } else if (color[next] == 1) {
+                cycle_end = node;
+                cycle_start = next;
+                return true;
+            }
+        }
+        color[node] = 2;
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        n = nextInt();
+        m = nextInt();
+
+        graph = new List[n + 1];
+        parent = new int[n + 1];
+        color = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int a, b;
+        for (int i = 0; i < m; i++) {
+            a = nextInt();
+            b = nextInt();
+            graph[a].add(b);
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (color[i] == 0 && dfs(i)) {
+                break;
+            }
+        }
+        
+        if (cycle_start == -1) {
+            sa.println("No hay ciclo");
+        } else {
+            sa.println("Si hay ciclo");
         }
 
         sa.close();
