de83470dacc02a5b07457090b455334e9d024d16
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-10-22 14:52:25 -0500

    fix

--- Java/utils/InputReader.java
+++ Java/utils/InputReader.java
@@ -1,160 +1,69 @@
-Mas rapido que el BufferedReader
+    
+    
+    //Mas rapido que el BufferedReader
+    // ##############################################################
+    // ##############################################################
 
-    // en = new InputReader("input.txt");
+    static int scan() throws IOException {
+        if (_index >= _total) {
+            _index = 0;
+            _total = in.read(buf);
+            if (_total <= 0) {
+                return -1;
+            }
+        }
+        return buf[_index++];
+    }
+
+    static String next() throws IOException {
+        int c;
+        for (c = scan(); c <= 32; c = scan())
+            ;
+        StringBuilder sb = new StringBuilder();
+        for (; c > 32; c = scan()) {
+            sb.append((char) c);
+        }
+        return sb.toString();
+    }
+
+    static int nextInt() throws IOException {
+        int c, val = 0;
+        for (c = scan(); c <= 32; c = scan())
+            ;
+        boolean neg = c == '-';
+        if (c == '-' || c == '+') {
+            c = scan();
+        }
+        for (; c >= '0' && c <= '9'; c = scan()) {
+            val = (val << 3) + (val << 1) + (c & 15);
+        }
+        return neg ? -val : val;
+    }
+
+    static long nextLong() throws IOException {
+        int c;
+        long val = 0;
+        for (c = scan(); c <= 32; c = scan())
+            ;
+        boolean neg = c == '-';
+        if (c == '-' || c == '+') {
+            c = scan();
+        }
+        for (; c >= '0' && c <= '9'; c = scan()) {
+            val = (val << 3) + (val << 1) + (c & 15);
+        }
+        return neg ? -val : val;
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    // para input/output con files
+    // in = new FileInputStream("input.txt");
     // sa = new BufferedWriter(new BufferedWriter(new FileWriter("output.txt")));
 
- static class InputReader {
-
-        InputStream stream;
-        byte[] buf = new byte[1024];
-        int curChar;
-        int numChars;
-        SpaceCharFilter filter;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        public InputReader() {
-            this.stream = System.in;
-        }
-
-        public InputReader(String file) throws IOException {
-            this.stream = new FileInputStream(file);
-        }
-
-        int read() {
-            if (numChars == -1) {
-                throw new InputMismatchException();
-            }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-
-                if (numChars <= 0) {
-                    return -1;
-                }
-            }
-            return buf[curChar++];
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-        int nextInt() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        long nextLong() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            long res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        double nextDouble() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            double res = 0;
-            while (!isSpaceChar(c) && c != '.') {
-                if (c == 'e' || c == 'E') {
-                    return res * Math.pow(10, nextInt());
-                }
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            }
-            if (c == '.') {
-                c = read();
-                double m = 1;
-                while (!isSpaceChar(c)) {
-                    if (c == 'e' || c == 'E') {
-                        return res * Math.pow(10, nextInt());
-                    }
-                    if (c < '0' || c > '9') {
-                        throw new InputMismatchException();
-                    }
-                    m /= 10;
-                    res += (c - '0') * m;
-                    c = read();
-                }
-            }
-            return res * sgn;
-        }
-
-        String next() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            StringBuilder res = new StringBuilder();
-            do {
-                res.appendCodePoint(c);
-                c = read();
-            } while (!isSpaceChar(c));
-            return res.toString();
-        }
-
-        boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-        }
-
-    }
+    static PrintWriter sa = new PrintWriter(new OutputStreamWriter(System.out));
+    static byte[] buf = new byte[2048];
+    static InputStream in = System.in;
+    static int _index, _total;
