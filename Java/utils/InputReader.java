68a1fa9916264f209365e20a9b0d2442a9c650b4
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-10-28 17:53:09 -0500

    IO

--- Java/utils/InputReader.java
+++ Java/utils/InputReader.java
@@ -1,69 +1,54 @@
     
     
-    //Mas rapido que el BufferedReader
-    // ##############################################################
-    // ##############################################################
-
-    static int scan() throws IOException {
-        if (_index >= _total) {
-            _index = 0;
-            _total = in.read(buf);
-            if (_total <= 0) {
-                return -1;
-            }
+    // Muy rapido para leer numeros
+    // ##########################################################
+    
+    static int scan()  {
+        if (idx_ >= total_) {
+            idx_ = 0;
+            try {total_ = in.read(buf);}
+            catch (IOException e) {e.printStackTrace();}
+            if (total_ <= 0) return -1;
         }
-        return buf[_index++];
+        return buf[idx_++];
     }
 
-    static String next() throws IOException {
+    static String next() {
         int c;
-        for (c = scan(); c <= 32; c = scan())
-            ;
-        StringBuilder sb = new StringBuilder();
-        for (; c > 32; c = scan()) {
-            sb.append((char) c);
-        }
-        return sb.toString();
+        for (c = scan(); c <= 32; c = scan());
+        sb_ = new StringBuilder();
+        for (; c > 32; c = scan())
+            sb_.append((char) c);
+        return sb_.toString();
     }
 
-    static int nextInt() throws IOException {
+    static int nextInt() {
         int c, val = 0;
-        for (c = scan(); c <= 32; c = scan())
-            ;
+        for (c = scan(); c <= 32; c = scan());
         boolean neg = c == '-';
-        if (c == '-' || c == '+') {
-            c = scan();
-        }
-        for (; c >= '0' && c <= '9'; c = scan()) {
+        if (c == '-' || c == '+') c = scan();
+        for (; c >= '0' && c <= '9'; c = scan()) 
             val = (val << 3) + (val << 1) + (c & 15);
-        }
         return neg ? -val : val;
     }
 
-    static long nextLong() throws IOException {
+    static long nextLong(){
         int c;
         long val = 0;
-        for (c = scan(); c <= 32; c = scan())
-            ;
+        for (c = scan(); c <= 32; c = scan());
         boolean neg = c == '-';
-        if (c == '-' || c == '+') {
-            c = scan();
-        }
-        for (; c >= '0' && c <= '9'; c = scan()) {
+        if (c == '-' || c == '+') c = scan();
+        for (; c >= '0' && c <= '9'; c = scan())
             val = (val << 3) + (val << 1) + (c & 15);
-        }
         return neg ? -val : val;
     }
 
-    static double nextDouble() throws IOException {
+    static double nextDouble() {
         return Double.parseDouble(next());
     }
 
-    // para input/output con files
-    // in = new FileInputStream("input.txt");
-    // sa = new BufferedWriter(new BufferedWriter(new FileWriter("output.txt")));
-
     static PrintWriter sa = new PrintWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb_;
     static byte[] buf = new byte[2048];
     static InputStream in = System.in;
-    static int _index, _total;
+    static int idx_, total_;
