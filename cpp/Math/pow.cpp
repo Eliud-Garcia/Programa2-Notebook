4e65f2acb0e073634d80c58365bae4753a1c3744
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-12-06 12:34:08 -0500

    pow

---
+++ cpp/Math/pow.cpp
@@ -0,0 +1,27 @@
+
+#define ll long long
+
+//a  =  a^b
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
