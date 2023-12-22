6fc36d0a10ab2e964ceacd7738d56c0a5a98baaf
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-12-21 20:10:46 -0500

    lca

--- Java/Graph/LCA binary lifting.java
+++ Java/Graph/LCA binary lifting.java
@@ -3,29 +3,29 @@
     //build = n log(n)
     //query = log(n)
     
-    static int n, log, q;
-    static List<Integer> tree[];
-    static int depth[];
+    static int n, m, q, log;
+    static List<Integer> g[];
+    static List<Integer> projects[];
+    static int deep[];
+    //static int times[]; // tiempo de descubrir el nodo en el dfs
     static int up[][];
+    static int cnt = 1;
 
-    static void dfs(int node, int p, int d) {
-        up[0][node] = p;
-        depth[node] = d;
-        for (int u : tree[node]) {
-            if (u == p)
-                continue;
-            depth[u] = depth[node] + 1;
-            dfs(u, node, d + 1);
+    static void dfs(int u, int p) {
+        for (int v : g[u]) {
+            if (v != p){
+                deep[v] = deep[u] + 1;
+                up[0][v] = u;
+                //times[v] = cnt++;
+                dfs(v, u);
+            } 
         }
     }
 
-    static void build(int n) {
-        for (int a[] : up) {
-            Arrays.fill(a, -1);
-        }
-        dfs(1, 1, 0);
-        for (int i = 1; i < log; i++) {
-            for (int u = 0; u < n; u++) {
+    static void build(int n, int root) {
+        dfs(root, root);
+        for (int i = 1; i <= log; i++) {
+            for (int u = 1; u < n; u++) {
                 if (up[i - 1][u] != -1) {
                     up[i][u] = up[i - 1][up[i - 1][u]];
                 }
@@ -34,52 +34,52 @@
     }
 
     static int lca(int u, int v) {
-        if (depth[u] > depth[v]) {
-            int x = u;
+        if (deep[u] > deep[v]) {
+            int tmp = u;
             u = v;
-            v = x;
+            v = tmp;
         }
-        int dis = depth[v] - depth[u];
+        int diff = deep[v] - deep[u];
         for (int i = log - 1; i >= 0; i--) {
-            if (((dis >> i) & 1) == 1) {
+            if((diff & (1 << i)) !=0){
                 v = up[i][v];
             }
         }
-        if (u == v)
-            return u;
+        if (u == v) return u;
         for (int i = log - 1; i >= 0; i--) {
             if (up[i][v] != up[i][u]) {
                 u = up[i][u];
                 v = up[i][v];
             }
         }
-        return up[0][v];
+        return up[0][u];
     }
 
     public static void main(String[] args) {
         n = nextInt();
-        tree = new List[n + 1];
-        depth = new int[n + 1];
+        g = new List[n + 1];
+        deep = new int[n + 1];
         log = (int) (ceil(log(n + 1) / log(2)));
         up = new int[log + 1][n + 1];
-        Arrays.setAll(tree, i -> new ArrayList<>());
-
+        Arrays.setAll(g, i -> new ArrayList<>());
+ 
         int a, b, c;
         for (int i = 0; i < n - 1; i++) {
             a = nextInt();
             b = nextInt();
-            tree[a].add(b);
-            tree[b].add(a);
+            g[a].add(b);
+            g[b].add(a);
         }
-        build(n + 1);
+        int root = 1;
+        build(n + 1, root);
         q = nextInt();
         while (q-- > 0) {
             a = nextInt();
             b = nextInt();
             c = nextInt();
             int ancestor = lca(a, b);
-            int disAB = depth[a] + depth[b] - (2 * depth[ancestor]);
-            int disA_lca = depth[a] - depth[ancestor];
+            int disAB = deep[a] + deep[b] - (2 * deep[ancestor]);
+            int disA_lca = deep[a] - deep[ancestor];
             if (disAB <= c) {
                 sa.print(b + "\n");
             } else {
@@ -103,7 +103,8 @@
                 }
             }
         }
-
+ 
         sa.close();
     }
+
     //https://codeforces.com/gym/102694/problem/C/
