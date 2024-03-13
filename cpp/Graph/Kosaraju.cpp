427224f9e05dbf83512de7fba090dcce5651e39e
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-03-12 21:52:24 -0500

    kosa

--- cpp/Graph/Kosaraju.cpp
+++ cpp/Graph/Kosaraju.cpp
@@ -11,20 +11,17 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef vector<bool> vb;
 typedef vector<ll> vll;
-
-//Kosaraju
-//numero de componentes fuertemente conexas = tag
 const int MAXN = (int)1e5+ 5;
 vi g[MAXN];
 vi rg[MAXN];
 vb vis(MAXN), id(MAXN);
 int tag;
+vector<vi> comp;
 void dfs1(int u, stack<int> &st){
     vis[u] = 1;
     for(int v : g[u]){
-        if(!vis[v]){
-            dfs1(v, st);
-        }
+        if(!vis[v]) dfs1(v, st);
+        
     }
     st.push(u);
 }
@@ -32,11 +29,11 @@ void dfs1(int u, stack<int> &st){
 void dfs2(int u){
     vis[u] = 1;
     id[u] = tag;
-    cout << u << " ";
+    comp.back().pb(u);
+    //u esta en la componente id[u]
     for(int v: rg[u]){
-        if(!vis[v]){
-            dfs2(v);
-        }
+        if(!vis[v]) dfs2(v);
+        
     }
 }
 int n, m;
@@ -45,7 +42,6 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
     cin >> n >> m;
 
     int a, b;
@@ -67,12 +63,20 @@ int main()
         int u = st.top(); st.pop();
         if(vis[u]) continue;
         tag++;
-        cout << "componente #" << tag << " : ";
+        comp.pb({});
         dfs2(u);
+    }
+
+    int cp = 1;
+    for(auto i : comp){
+        cout << "componente #" << cp <<" : ";
+        for(auto j: i){
+            cout << j << " ";
+        }
         cout << ln;
     }
-    
 
-    
+   
+
     return 0;
 }
