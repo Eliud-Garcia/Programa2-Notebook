058af94130cc6057a394327b507039e2a539ec88
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-03-22 15:52:11 -0500

    cpp

--- cpp/Math/Math utils.cpp
+++ cpp/Math/Math utils.cpp
@@ -6,11 +6,20 @@ int resta(int a, int b){
     return ((a % MOD) - (b % MOD)) % MOD;
 } 
 int mult(int a, int b){
-    reuturn ((a % MOD) * (b % MOD)) % MOD;
+    return ((a % MOD) * (b % MOD)) % MOD;
 } 
 int div(int a, int b){
     return ((a % MOD) * (modInverse(b, MOD))) % MOD;
 }
 
+//GCD    
+ll gcd(ll a, ll b) {
+    return b == 0 ? (a < 0 ? -a : a) : gcd(b, a % b);
+}
+//LCM
+ll lcm(ll a, ll b) {
+    long lcm = (a / gcd(a, b)) * b;
+    return lcm > 0 ? lcm : -lcm;
+}
 //cuantas veces B puede restar a A
 // x = floor(A/B + 0.0)
