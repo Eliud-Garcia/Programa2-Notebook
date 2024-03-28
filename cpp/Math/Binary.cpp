56aeb03227101079ec74a1b867fe1eef6f496c68
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-03-28 13:25:15 -0500

    cpp

--- cpp/Math/Binary.cpp
+++ cpp/Math/Binary.cpp
@@ -1,3 +1,4 @@
+
 //numeros desde [0, n] en binario
 vi pre(int n){
     vi bin;
@@ -14,8 +15,35 @@ vi pre(int n){
                 s+= (bit ? "1" : "0");
             }
         }
-        if(sz(s) > 5) return bin;
-        bin.pb(stoi(s));
+        int X = stoi(s);
+        bin.pb(X);
     }
     return bin;
+}
+//dado un N decir si se puede formar
+//por la multiplicacion de numeros
+//en su representacion binaria
+//N = bin * bin
+//121 = 11 * 11
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    vi bin = pre(32);
+    int MAX = 1e5 + 10;
+    vb sol(MAX, 0);
+    sol[1] = 1;
+    forab(i, 1, MAX){
+        if(sol[i]){
+            for(int x : bin){
+                if((ll) i * x < MAX) sol[i*x] = 1;
+            }
+        }
+    }
+    while(t--){
+        ll n; cin >> n;
+        cout << (sol[n] ? "YES" :"NO" ) <<ln;
+    }   
+    return 0;
 }
