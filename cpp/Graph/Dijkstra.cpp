a549a8c8a940053e9be151560596352e44be95a0
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-04-16 14:59:34 -0500

    cpp

--- cpp/Graph/Dijkstra.cpp
+++ cpp/Graph/Dijkstra.cpp
@@ -1,98 +1,82 @@
-
-
-
-const long long INF = LLONG_MAX;
-
+const ll oo = LLONG_MAX;
 struct edge {
-    int v;
-    long long w;
-
-    edge(int node, long long weight)
-        : v(node)
-        , w(weight)
-    {
-    }
-    bool operator<(const edge& x) const
-    {
-        return x.w < w;
-    }
+  int v;
+  ll w;
+  edge(int node, ll weight): v(node), w(weight){}
+  bool operator<(const edge& x) const{
+    return x.w < w;
+  }
 };
+const int MAXN = 2e5 + 5;
+vector<edge> g[MAXN];
+vi par;
+vb vis;
+int n, m;
 
-vector<vector<edge>> g;
-vector<long long> dis;
-vector<int> par;
-vector<bool> vis;
-
-bool dijkstra(int s, int t)
-{
-    priority_queue<edge> pq;
-
-    pq.push(edge(s, 0));
-    dis[s] = 0;
-    par[s] = -1;
-
-    while (!pq.empty()) {
-        edge x = pq.top();
-        pq.pop();
-        int u = x.v;
-
-        if (u == t) {
-            return true;
-        }
-
-        vis[u] = true;
-
-        for (edge next : g[u]) {
-            int v = next.v;
-            long long w = next.w;
-
-            if (!vis[v] && dis[u] + w < dis[v]) {
-                dis[v] = dis[u] + w;
-                pq.push(edge(v, dis[v]));
-                par[v] = u;
-            }
-        }
-    }
-
-    return false;
+vll dijkstra(int s){
+  priority_queue<edge> pq;
+	vll dis(n + 5, oo);
+	pq.push(edge(s, 0));
+	dis[s] = 0;
+	par[s] = -1;
+	while (!pq.empty()) {
+		edge x = pq.top();
+		pq.pop();
+		int u = x.v;
+		vis[u] = true;
+		for (edge next : g[u]) {
+			int v = next.v;
+			ll  w = next.w;
+			if (!vis[v] && dis[u] + w < dis[v]) {
+				dis[v] = dis[u] + w;
+				par[v] = u;
+				pq.push(edge(v, dis[v]));
+			}
+		}
+	}
+	return dis;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+int main(){
+	cin >> n >> m;
+	par = vi(n + 5, -1);
+	vis = vb(n + 5, 0);
+	int a, b;
+	ll w;
+	forn (i, m) {
+		cin >> a >> b >> w;
+		g[a].pb(edge(b, w));
+		g[b].pb(edge(a, w));
+	}
+	dijkstra(1);
+	if (vis[n]) {
+		vi path;
+		for (b = n; b != -1; b = par[b]) {
+			path.pb(b);
+		}
+		rforn (i, sz(path)) {
+			cout << path[i] << " ";
+		}
+		cout << ln;
+	} else {
+		cout << "-1" << ln;
+	}
+	return 0;
+}
 
-    int n, m;
-    cin >> n >> m;
-
-    g.resize(n + 3);
-    dis.assign(n + 3, INF);
-    par.assign(n + 3, -1);
-    vis.assign(n + 3, false);
-
-    int a, b;
-    long long w;
-
-    for (int i = 0; i < m; i++) {
-        cin >> a >> b >> w;
-        g[a].push_back(edge(b, w));
-        g[b].push_back(edge(a, w));
-    }
-
-    if (dijkstra(1, n)) {
-        vector<int> path;
-        for (b = n; b != -1; b = par[b]) {
-            path.push_back(b);
-        }
-
-        for (int i = path.size() - 1; i >= 0; i--) {
-            cout << path[i] << " ";
-        }
-        cout << ln;
-    } else {
-        cout << "-1" << ln;
-    }
-
-    return 0;
+//validar si una arista
+//esta en el camino mas corto
+//desde (a, b)
+vll da = dijkstra(a);
+ll minDis = da[b];
+vll db = dijkstra(b);
+for (edge e : aristas) {
+  //edge is on shortest path
+  if(disSource[e.u] + e.w + disDest[e.v] == minDis){
+    continue;
+  }
+  //edge is on shortest path
+  if(disSource[e.v] + e.w + disDest[e.u] == minDis){
+    continue;
+  }
 }
