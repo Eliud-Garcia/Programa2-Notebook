f7695904501f0c327512ade9b6b0252c319ff2f7
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-04-27 14:11:39 -0500

    div

--- cpp/Math/Divisors of a number.cpp
+++ cpp/Math/Divisors of a number.cpp
@@ -1,16 +1,11 @@
 //todos los divisores n en O(sqrt(n))
-vi divisors(ll n){
-  vi div;
+vll divisors(ll n){
+  vll div;
   for (ll i = 1; i * i <= n; i++) {
     if (n % i == 0) {
-      if (i * i == n) {
-        div.pb(i);
-      } else {
-        div.pb(i);
-        if (i != 1) {
-          div.pb(n / i);
-        }
-      }
+      div.pb(i);
+      if ((n/i) != i) 
+        div.pb(n / i);
     }
   }
   return div;
