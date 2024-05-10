fbc9b95d5e417971ee2f5d7d3ded9db3eaaac9da
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-05-10 15:04:39 -0500

    cpp

--- cpp/Data Structures/Fenwick Tree.cpp
+++ cpp/Data Structures/Fenwick Tree.cpp
@@ -1,39 +1,41 @@
 
 template <typename T>
 struct BIT {
-  vector<T> ft;
-  int N;
-  BIT(int n): ft(n + 1), N(n){}
+   vector<T> ft;
+   int N;
+   BIT(int n): ft(n + 1), N(n) {}
 
-  BIT(const vector<T>& a): ft(sz(a) + 1), N(sz(a)){
-    forn(i, sz(a)) { upd(i + 1, a[i]); }
-  }
-
-  T qry(int i){
-    T ans = 0;
-    for (; i; i -= i & -i) ans += ft[i];
-    return ans;
-  }
-
-  T qry(int l, int r){
-    return qry(r) - qry(l - 1);
-  }
-
-  void upd(int i, T v){
-    for (; i < sz(ft); i += i & -i) ft[i] += v;
-  }
-  //This is equivalent to calculating 
-  //lower_bound on prefix sums array
-  int bit_search(int v){ // O(log(N))
-    int sum = 0;
-    int pos = 0;
-    int LOGN = int(log2(N));
-    for (int i = LOGN; i >= 0; i--) {
-      if (pos + (1 << i) < N && sum + ft[pos + (1 << i)] < v) {
-        sum += ft[pos + (1 << i)];
-        pos += (1 << i);
+   BIT(const vector<T>& a): ft(sz(a) + 1), N(sz(a)) {
+      forn(i, sz(a)) {
+         upd(i + 1, a[i]);
       }
-    }
-    return pos + 1; 
-  }
+   }
+
+   T qry(int i) {
+      T ans = 0;
+      for (; i; i -= i & -i) ans += ft[i];
+      return ans;
+   }
+
+   T qry(int l, int r) {
+      return qry(r) - qry(l - 1);
+   }
+
+   void upd(int i, T v) {
+      for (; i < sz(ft); i += i & -i) ft[i] += v;
+   }
+   //This is equivalent to calculating
+   //lower_bound on prefix sums array
+   int bit_search(int v) { // O(log(N))
+      int sum = 0;
+      int pos = 0;
+      int LOGN = int(log2(N));
+      for (int i = LOGN; i >= 0; i--) {
+         if (pos + (1 << i) < N && sum + ft[pos + (1 << i)] < v) {
+            sum += ft[pos + (1 << i)];
+            pos += (1 << i);
+         }
+      }
+      return pos + 1;
+   }
 };
--- cpp/Dynamic Programming/Counting tilings.cpp
+++ cpp/Dynamic Programming/Counting tilings.cpp
@@ -4,38 +4,38 @@ const int MOD = 1e9 + 7;
 int dp[1001][1<<10];
 int n, m;
 
-void fill_colum(int column, int idx, int cur_mask, int next_mask){
-    if (idx == n) { // he llenado toda la columna
-        dp[column + 1][next_mask] = (dp[column + 1][next_mask] + dp[column][cur_mask]) % MOD;
-        return;
-    }
-    if ((cur_mask) & (1 << idx)) { // si el tile actual esta full
-        fill_colum(column, idx + 1, cur_mask, next_mask);
-    } else {
-        // horizontal
-        fill_colum(column, idx + 1, cur_mask, next_mask | (1 << idx));
-        // vertical
-        if (idx + 1 < n && (!(cur_mask & (1 << (idx + 1))))) {
-            fill_colum(column, idx + 2, cur_mask, next_mask);
-        }
-    }
+void fill_colum(int column, int idx, int cur_mask, int next_mask) {
+   if (idx == n) { // he llenado toda la columna
+      dp[column + 1][next_mask] = (dp[column + 1][next_mask] + dp[column][cur_mask]) % MOD;
+      return;
+   }
+   if ((cur_mask) & (1 << idx)) { // si el tile actual esta full
+      fill_colum(column, idx + 1, cur_mask, next_mask);
+   } else {
+      // horizontal
+      fill_colum(column, idx + 1, cur_mask, next_mask | (1 << idx));
+      // vertical
+      if (idx + 1 < n && (!(cur_mask & (1 << (idx + 1))))) {
+         fill_colum(column, idx + 2, cur_mask, next_mask);
+      }
+   }
 }
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
-    dp[0][0] = 1;
-    forn(column, m){ // todas las coumnas
-        forn(mask, (1 << n)){ // todas las posibles mask
-            if (dp[column][mask] > 0) {
-                // hay forma de llenar la i-esima columna
-                fill_colum(column, 0, mask, 0);
-            }
-        }
-    }
-    cout << dp[m][0] << ln;
+int main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   cin >> n >> m;
+   dp[0][0] = 1;
+   forn(column, m) { // todas las coumnas
+      forn(mask, (1 << n)) { // todas las posibles mask
+         if (dp[column][mask] > 0) {
+            // hay forma de llenar la i-esima columna
+            fill_colum(column, 0, mask, 0);
+         }
+      }
+   }
+   cout << dp[m][0] << ln;
 
-    return 0;
+   return 0;
 }
