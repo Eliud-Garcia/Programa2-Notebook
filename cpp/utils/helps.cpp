781a2bafd12cf44841e05f5654358dcf7bbe2c23
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-05-13 19:49:45 -0500

    cpp

--- cpp/utils/helps.cpp
+++ cpp/utils/helps.cpp
@@ -14,5 +14,14 @@ set<pii, decltype(&cmp)> q(&cmp);
 function<ll(int, int, int)> suma = [&](int a, int b, int c) {
   return ll(a + b + c);
 };
-
 cout << suma(11, 11, 1) << ln;
+
+//input / output
+string x;
+getline(cin, x);//lee linea completa
+
+//imprime long double con 6 decimales
+printf("%.6LF\n", value);//long double
+
+
+
