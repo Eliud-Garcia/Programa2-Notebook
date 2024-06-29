5b50d4761ea4fe9bb6ae3a1744b3b3fb9021352f
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-06-29 17:01:17 -0500

    dinic

--- cpp/Flows/Dinic.cpp
+++ cpp/Flows/Dinic.cpp
@@ -1,18 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ln '\n'
-#define all(x) x.begin(), x.end()
-#define forn(i, n) for(int i = 0; i < n; i++)
-#define forab(i, a, b) for (int i = a; i < b; i++)
-#define pb push_back
-#define sz(x) int(x.size())
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<bool> vb;
-typedef vector<ll> vll;
-typedef vector<pair<int, int>> vii;
 
 // Corte minimo: vertices con dist[v]>=0 (del lado de src) VS.  dist[v]==-1 (del lado del dst)
 // Para el caso de la red de Bipartite Matching (Sean V1 y V2 los conjuntos mas proximos a src y dst respectivamente):
@@ -22,6 +7,8 @@ typedef vector<pair<int, int>> vii;
 // Max Clique: construir la red de G complemento (debe ser bipartito!) y encontrar un Max Independet Set
 // Min Edge Cover: tomar las aristas del matching + para todo vertices no cubierto hasta el momento, tomar cualquier arista de el
 // Complexity O(V^2*E)
+
+typedef vector<pair<int, int>> vii;
 
 const ll inf = 1e18;
 struct edge {
@@ -39,7 +26,7 @@ struct Dinic{
     g[s].pb(edge(t, sz(g[t]), cap));
     g[t].pb(edge(s, sz(g[s])-1, 0));
   }
-  
+
   bool bfs(){
     dis.assign(n, -1), dis[s] = 0;
     int qt = 0;
@@ -74,7 +61,7 @@ struct Dinic{
     }
     max_flow += cur_flow;
     // todos los nodos con dis[u]!=-1 vs los que tienen dis[v]==-1 forman el min-cut, (u,v)
-    return max_flow; 
+    return max_flow;
   }
   vii min_cut(){
     maxFlow();
@@ -87,36 +74,33 @@ struct Dinic{
     return cut;
   }
 };
- 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    string s; 
-    int n;
-    cin >> s >> n;
-    vector<set<char>> cubes(n);
-    forn(i, n){
-        forn(j, 6){
-            char c; cin >> c;
-            cubes[i].insert(c);
-        }
+
+int main() {
+  string s;
+  int n;
+  cin >> s >> n;
+  vector<set<char>> cubes(n);
+  forn(i, n) {
+    forn(j, 6) {
+      char c;
+      cin >> c;
+      cubes[i].insert(c);
     }
-    int nodes = sz(s) + n + 2; //cantidad nodos
-    Dinic dd(nodes);
-    // S -> s[i]
-    forn(i, sz(s)) dd.addEdge(dd.s, i, 1);
-    //cubes[i] -> T
-    forn(i, n) dd.addEdge(sz(s) + i, dd.t, 1);
-    forn(i, sz(s)){
-        forn(j, n){
-            if(cubes[j].count(s[i])){
-                dd.addEdge(i, sz(s) + j, 1);
-            }
-        }
+  }
+  int nodes = sz(s) + n + 2; //cantidad nodos
+  Dinic dd(nodes);
+  // S -> s[i]
+  forn(i, sz(s)) dd.addEdge(dd.s, i, 1);
+  //cubes[i] -> T
+  forn(i, n) dd.addEdge(sz(s) + i, dd.t, 1);
+  forn(i, sz(s)) {
+    forn(j, n) {
+      if(cubes[j].count(s[i])) {
+        dd.addEdge(i, sz(s) + j, 1);
+      }
     }
-    ll flow = dd.maxFlow();
-    cout << (flow == sz(s) ? "YES" : "NO") <<ln;
-    return 0;
+  }
+  ll flow = dd.maxFlow();
+  cout << (flow == sz(s) ? "YES" : "NO") <<ln;
+  return 0;
 }
