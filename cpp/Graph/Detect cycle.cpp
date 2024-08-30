809653234ea78ee8b3ee0ee72b2b278a36a39966
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2024-08-30 14:45:06 -0500

    cycles

--- cpp/Graph/Detect cycle.cpp
+++ cpp/Graph/Detect cycle.cpp
@@ -1,57 +1,61 @@
 
 //directed graph
+int n, m;
 const int MAXN = 1e5 + 5;
 vi g[MAXN];
-vi parent, color;
-int n, m;
-int cycle_start = -1;
-int cycle_end = -1;
+vi par(MAXN, -1);
+vi color(MAXN, 0);
+int inicio = -1, fin = -1;
+vi cycle;
 
-bool dfs(int node) {
-  color[node] = 1;
-  for(int next: g[node]) {
-    if(color[next] == 0) {
-      parent[next] = node;
-      if(dfs(next)) {
-        return 1;
+bool dfs(int u){
+  color[u] = 1;
+  for(int v: g[u]){
+    if(color[v] == 0){
+      par[v] = u;
+      if(dfs(v)) return 1;
+    }else if(color[v] == 1){
+      if(inicio != -1 && fin != -1) return 1;
+      inicio = u;
+      fin = v;
+      vi aux;
+      int cur = u;
+      aux.pb(cur);
+      while(cur != v){
+        cur = par[cur];
+        aux.pb(cur);
       }
-    } else if(color[next] == 1) {
-      cycle_end = node;
-      cycle_start = next;
+      aux.pb(u);
+      if(sz(aux) >= 2){
+        reverse(all(aux));
+        cycle = aux;
+      }
       return 1;
     }
   }
-  color[node] = 2;
+  color[u] = 2;
   return 0;
 }
 
 int main() {
   cin >> n >> m;
-  parent = vi(n + 1);
-  color = vi(n + 1);
   int a, b;
-  forn(i, m) {
+  forn(i, m){
     cin >> a >> b;
     g[a].pb(b);
   }
-  forab(i, 1, n + 1) {
-    if(color[i] == 0 && dfs(i)) break;
+  forab(i, 1, n + 1){
+    if(color[i] == 0 && dfs(i))break;
+    if(sz(cycle) > 0) break;
   }
-  if(cycle_start == -1) {
-    //no hay ciclo
-  } else {
-    //si hay ciclo
-    vi path;
-    path.pb(cycle_end);
-    for (int i = cycle_end; i != cycle_start; i = parent[i]) {
-      path.pb(parent[i]);
+  if(sz(cycle) == 0){
+    puts("IMPOSSIBLE");
+  }else{
+    printf("%d\n", sz(cycle));
+    for(int i : cycle){
+      printf("%d ", i);
     }
-    path.pb(cycle_end);
-    cout << sz(path) << ln;
-    for (int u : path) {
-      cout << u <<" ";
-    }
-    cout << ln;
+    puts("");
   }
   return 0;
 }
@@ -95,6 +99,6 @@ int main() {
   if(cycle_start == -1) //no hay ciclo
     else //hay ciclo
       //se puede recorrer el ciclo
-      //como en la implementacion del directed graph
+      //usando el array de padres
       return 0;
 }
