855fb8df8160f62d117b8be221f10d20f1e5d7aa
eliud <jhonniereliudgarciacapera@gmail.com>
2024-11-13 13:42:10 -0500

    geometry

--- cpp/Geometry/geometry.cpp
+++ cpp/Geometry/geometry.cpp
@@ -9,3 +9,21 @@ bool sameLine(int p1, int p2, int p3) {
   ll v2 =  X[p2] * Y[p1] + X[p3] * Y[p2] + X[p1] * Y[p3];
   return v1 == v2;
 }
+
+
+
+/*
+razones trigonometricas
+
+CO = cateto opuesto
+CA = cateto adyacente
+H = hipotenusa
+
+sin(angulo) = CO / H
+cos(angulo) = CA / H
+tan(angulo) = CO / CA
+
+sosecante(angulo) = H / CO
+secante(angulo) = H / CA
+cotangente(angulo) = CA / CO 
+*/
