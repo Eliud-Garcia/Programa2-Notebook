a7c9a0e3dc919e27856af9d978aec01e919f5ea5
Eliud-Garcia <jhonniereliudgarciacapera@gmail.com>
2025-06-01 18:17:26 -0500

    geometry

--- cpp/Geometry/geometry.cpp
+++ cpp/Geometry/geometry.cpp
@@ -1,17 +1,5 @@
 
 
-//cordenada (x, y) de cada punto
-vi X;
-vi Y;
-//valida si los 3 puntos estan en la misma linea
-bool sameLine(int p1, int p2, int p3) {
-  ll v1 = X[p1] * Y[p2] + X[p2] * Y[p3] + X[p3] * Y[p1];
-  ll v2 =  X[p2] * Y[p1] + X[p3] * Y[p2] + X[p1] * Y[p3];
-  return v1 == v2;
-}
-
-
-
 /*
 razones trigonometricas
 
@@ -27,3 +15,78 @@ sosecante(angulo) = H / CO
 secante(angulo) = H / CA
 cotangente(angulo) = CA / CO 
 */
+
+using ld = long double;
+
+struct point {
+    ll x, y, id;
+    point(ll a = 0, ll b = 0, int i = -1) {
+        x = a;
+        y = b;
+        id = i;
+    }
+
+    point operator -(const point b) {
+        return point(x - b.x, y - b.y, -1);
+    }
+
+    //cross(a, b) producto escalar
+    ll operator ^(const point b) {
+        return x * b.y - y * b.x;
+    }
+
+    ll operator *(const point b) {
+        return x * b.x + y * b.y;
+    }
+};
+
+ld norm(point a) { // Modulo
+    return sqrt(a * a);
+}
+
+// ======= POLAR SORT =======
+int ret[2][2] = {{3, 2}, {4, 1}};
+inline int quad(point p) {
+    return ret[p.x >= 0][p.y >= 0];
+}
+
+//points sorted in counterclockwise order
+bool comp(point a, point b) { // ccw
+    int qa = quad(a), qb = quad(b);
+    return (qa == qb ? (a ^ b) > 0 : qa < qb);
+}
+
+// ======= POLAR SORT =======
+
+// ang(a1,b1) <= ang(a2,b2)
+bool angle_less(point a1, point b1, point a2, point b2) {
+    point p1((a1 * b1), abs((a1 ^ b1)));
+    point p2((a2 * b2), abs((a2 ^ b2)));
+    return (p1 ^ p2) <= 0;
+}
+
+int main() {
+    //dados n vectores
+    //todos con origen en (0, 0)
+    //hallar dos vectores  diferentes
+    //que forman el angulo minimo
+    int n;
+    cin >> n;
+    vector<point> pts(n);
+
+    forn(i, n) {
+        ll x, y;
+        cin >> x >> y;
+        pts[i] = point(x, y, i + 1);
+    }
+
+    sort(all(pts), comp);
+    array<point, 2> ans = {pts[0], pts[n - 1]};
+    forab(i, 1, n) {
+        if(angle_less(ans[0], ans[1], pts[i], pts[i - 1])) {
+            ans = {pts[i], pts[i - 1]};
+        }
+    }
+    cout << ans[0].id << " " << ans[1].id << ln;
+    return 0;
+}
