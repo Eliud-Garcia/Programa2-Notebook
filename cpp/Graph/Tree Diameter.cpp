22aa3ecbe0a9baced39470ec660dae0b4b8e2b38
Eliud-Garcia <jhonniereliudgarciacapera@gmail.com>
2025-07-16 13:02:04 -0500

    diameter

--- cpp/Graph/Tree Diameter.cpp
+++ cpp/Graph/Tree Diameter.cpp
@@ -1,43 +1,24 @@
-const int MAXN = 3e5 + 5;
+
+using ii = pair<int, int>;
+
+int n;
+const int MAXN = 2e5 + 5;
 vi g[MAXN];
-vb vis(MAXN, 0);
-vi dis(MAXN, 0);
-int n, m;
-int nodoLejano, maxDis;
-void bfs(int src) {
-  vis = vb(n + 1, 0);
-  dis = vi(n + 1);
-  queue<int> q;
-  vis[src] = 1;
-  nodoLejano = src;
-  q.push(src);
-  while(!q.empty()) {
-    int u = q.front();
-    q.pop();
-    if(dis[u] > maxDis) {
-      nodoLejano = u;
-      maxDis = dis[u];
+
+ii dfs(int u, int p){
+    ii ans = make_pair(0, u);
+    for(int v: g[u]){
+        if(v == p) continue;
+        ans = max(ans, dfs(v, u));
     }
-    for(int v: g[u]) {
-      if(!vis[v]) {
-        vis[v] = 1;
-        dis[v] = dis[u] + dis[v] + 1;
-        q.push(v);
-      }
-    }
-  }
+    ans.first++;
+    return ans;
 }
+
 int main() {
-  cin >> n;
-  int a, b;
-  forn(i, n - 1) {
-    cin >> a >> b;
-    g[a].pb(b);
-    g[b].pb(a);
-  }
-  maxDis = 0;
-  bfs(1);
-  bfs(nodoLejano);
-  cout << maxDis << ln;
-  return 0;
+    auto [dis, nodo] = dfs(1, 0);
+    auto [diam, raiz] = dfs(nodo, 0);
+
+    cout << diam - 1 << ln;
+    return 0;
 }
