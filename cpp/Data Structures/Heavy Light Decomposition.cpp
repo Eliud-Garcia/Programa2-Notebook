170088c773d7d353070c28f7c8ffeb290ca2d86f
Eliud-Garcia <jhonniereliudgarciacapera@gmail.com>
2025-09-16 16:39:50 -0500

    hld

--- cpp/Data Structures/Heavy Light Decomposition.cpp
+++ cpp/Data Structures/Heavy Light Decomposition.cpp
@@ -1,67 +1,83 @@
+//Segtree lazy code here
 
-//Segtree code here
-
-const int MAXN = 2e5 + 5;
+const int MAXN = 2e5  + 10;
 vi g[MAXN];
-int val[MAXN]; // value of each node
 
-vi st_val(MAXN, 0);
-STree<int> st(st_val);
+int val[MAXN]; // value of each node
+vll st_val(MAXN); // STree array
+STree<ll> st(MAXN); // segtree
 
 struct HLD {
-    int n, timer;
-    vi in, out, hp, tam;
-    vi deep, par;
+	vi in, tam, par, hp;
+	int timer;
 
-    HLD(int v, int n) {
-        this->n = n;
+	HLD(int root, int n){
         timer = 0;
         in.resize(n); //tin
-        out.resize(n); //tout
-        hp.resize(n); //heavy path
         tam.resize(n); //subtree size
-        deep.resize(n); //depth
-        par.resize(n); // parent
+        par.resize(n); //parent
+        hp.resize(n); //heavy path
 
-        dfs_sz(v, v);
-        hp[v] = v;
-        dfs_hld(v, v);
-    }
+        build_hld(root);
+        st.build(1, 0, tam(st_val) - 1, st_val);
+	}
 
-    void dfs_sz(int u, int p){
-        tam[u] = 1;
-        par[u] = p;
-        deep[u] = deep[p] + 1;
-        for (auto &v : g[u]) {
-            if(v == p) continue;
-            dfs_sz(v, u);
-            tam[u] += tam[v];
-            if (tam[v] > tam[g[u][0]])
-                swap(v, g[u][0]);
-        }
+    //save all values first in st_val and then build segtree
+    //or keep updating the segment tree
 
-    }
+	void build_hld(int u, int p = -1, int f = 1) {
+		st_val[in[u] = timer++] = val[u];
+        //st.upd(pos[k], pos[k], val[k]); -----
+		tam[u] = 1;
+		for (auto& v : g[u]) if (v != p) {
+			par[v] = u;
+			hp[v] = (v == g[u][0] ? hp[u] : v);
+			build_hld(v, u, f);
+			tam[u] += tam[v];
 
-    void dfs_hld(int u, int p) {
-        in[u] = timer++;
-        st.upd(in[u], val[u]);
-        for (auto &v : g[u]) {
-            if(v == p) continue;
-            hp[v] = v == g[u][0] ? hp[u] : v;
-            dfs_hld(v, u);
-        }
-        out[u] = timer;
-    }
+			if (tam[v] > tam[g[u][0]] || g[u][0] == p) swap(v, g[u][0]);
+		}
+		if (p * f == -1) build_hld(hp[u] = u, -1, timer = 0);
+	}
 
-    int path(int a, int b) { // query on path
-        int ans = 0;
-        while(hp[a] != hp[b]) {
-            if(deep[hp[a]] < deep[hp[b]])swap(a, b);
-            ans = max(ans, st.query(in[hp[a]], in[a]));
-            a = par[hp[a]];
-        }
-        if(deep[a] > deep[b]) swap(a, b);
-        ans = max(ans, st.query(in[a], in[b]));
-        return ans;
-    }
+	ll query_path(int a, int b) {
+        //if (a == b) return 0;
+		if (in[a] < in[b]) swap(a, b);
+
+		if (hp[a] == hp[b]){
+            return st.query(in[b], in[a]); // in[b] + 1 if the value is on the edge
+		}
+		return st.query(in[hp[a]], in[a]) + query_path(par[hp[a]], b);
+	}
+    
+	void update_path(int a, int b, int x) {
+        //if(a == b) return;
+		if (in[a] < in[b]) swap(a, b);
+
+		if (hp[a] == hp[b]){
+            return (void) st.upd(in[b], in[a], x); // in[b] + 1 if the value is on the edge
+		}
+
+		st.upd(in[hp[a]], in[a], x);
+		update_path(par[hp[a]], b, x);
+	}
+
+	ll query_subtree(int a) {
+		return st.query(in[a], in[a] + tam[a] - 1);
+	}
+
+	void update_subtree(int a, int x) {
+		st.upd(in[a], in[a] + tam[a] - 1, x);
+	}
+
+	int lca(int a, int b) {
+		if (in[a] < in[b]) swap(a, b);
+		return hp[a] == hp[b] ? b : lca(par[hp[a]], b);
+	}
 };
+
+/*
+If the value is on the edge, 
+the node that stores the value is the one at the greater depth, 
+and in queries and updates the higher one is excluded.
+*/
