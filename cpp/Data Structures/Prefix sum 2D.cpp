66b6213a69ad5d883c2447877db4b4f7456cfb9f
Eliud Garcia Ubuntu <jhonniereliudgarciacapera@gmail.com>
2025-10-09 12:19:37 -0500

    prefix sum 2d

---
+++ cpp/Data Structures/Prefix sum 2D.cpp
@@ -0,0 +1,37 @@
+
+
+const int MAXN = 1005;
+int a[MAXN][MAXN];
+int pref[MAXN][MAXN];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            pref[i][j] =
+                pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + a[i][j];
+        }
+    }
+    int q;
+    cin >> q;
+    while (q--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        int ans = 0;
+        // for (int i = x1; i <= x2; i++) {
+        //   for (int j = y1; j <= y2; j++) {
+        //     ans += a[i][j];
+        //   }
+        // }
+        ans = pref[x2][y2] - pref[x1 - 1][y2] - pref[x2][y1 - 1] +
+              pref[x1 - 1][y1 - 1];
+        cout << ans << ln;
+    }
+    return 0;
+}
