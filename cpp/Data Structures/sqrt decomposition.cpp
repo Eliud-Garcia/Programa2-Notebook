3b427c5955353861478eba5b1c8440befb685001
Eliud Garcia Ubuntu <jhonniereliudgarciacapera@gmail.com>
2025-12-03 18:15:36 -0500

    sq

--- cpp/Data Structures/sqrt decomposition.cpp
+++ cpp/Data Structures/sqrt decomposition.cpp
@@ -1,67 +1,89 @@
 
-//minimum range queries with updates
-
-const int oo = 1e9 + 50;
-const int MAXN = 2e5 + 5;
-
-const int S = sqrt(MAXN);//block size
-const int B = MAXN / S + 1; //# de bloques
-
-int a[MAXN];
-int blocks[B];
-
 int get_idx(int i){
     //es el indice donde inicia el
     //bloque en el que cae i
     return (i / S) * S;
 }
 
+int n, q;
+const int MAXN = 1e5;
+const int S = 317; //sqrt(n)
+const int B = MAXN / S + 1;
+
+int a[MAXN];
+vector<int> blk[B];
+int sum[B];
+
+//agrega x a todos en el rango [l, r]
+void add(int l, int r, int x){
+    l--, r--;
+    int i = l;
+    while(i <= r){
+        if(i % S == 0 && i + S < r){
+            sum[i / S] += x;
+            i += S;
+        }else{
+            auto& b = blk[i / S];
+            auto it = lower_bound(all(b), a[i]);
+            b.erase(it);
+
+            a[i] += x;
+            b.insert(lower_bound(all(b), a[i]), a[i]);
+            i++;
+        }
+    }
+}
+
+//valida si x está en el rango l, r
+int qry(int l, int r, int x){
+    l--, r--;
+    int ans = 0;
+    int i = l;
+    while(i <= r){
+        if(i % S == 0 && i + S < r){
+            auto& b = blk[i / S];
+            int to_search = x - sum[i / S];
+            auto it = lower_bound(all(b), to_search);
+            if(it != b.end() && *it == to_search){
+                ans++;
+            }
+            i += S;
+        }else{
+            ans += (a[i] + sum[i / S] == x);
+            i++;
+        }
+    }
+    return ans;
+}
+
 int main() {
-    int n, q;
     cin >> n >> q;
     forn(i, n) cin >> a[i];
 
-    fill(blocks, blocks + B, oo);
-
+    int mx = 0;
     forn(i, n){
-        blocks[i / S] = min(blocks[i / S], a[i]);
+        blk[i / S].push_back(a[i]);
+        sum[i / S] = 0;
+        mx = max(mx, i / S);
     }
-
+    for(int i = 0; i <= mx; i++) {
+        sort(all(blk[i]));
+    }
     while(q--){
-        int o;
-        cin >> o;
-        if(o == 1){
-            //modify
-            int id, v;
-            cin >> id >> v;
-            id--;
-            a[id] = v;
-            blocks[id / S] = oo;
-
-            int l = get_idx(id);
-            int r = min(n, l + S);
-
-            forab(i, l, r){
-                blocks[i / S] = min(blocks[i / S], a[i]);
-            }
-
+        char c;
+        int l, r, x;
+        cin >> c;
+        cin >> l >> r >> x;
+        if(c == '+'){
+            add(l, r, x);
         }else{
-            int l, r;
-            cin >> l >> r;
-            l--, r--;
-            int mn = oo;
-            int i = l;
-            while(i <= r){
-                if(i % S == 0 && i + S < r){
-                    mn = min(mn, blocks[i / S]);
-                    i += S;
-                }else{
-                    mn = min(mn, a[i]);
-                    i++;
-                }
-            }
-            cout << mn << ln;
+            int ans = qry(l, r, x);
+            cout << (ans ? "YES" : "NO") << ln;
         }
     }
     return 0;
 }
+
+//n = 1e5
+//q = 1e5
+//time = 3562 ms
