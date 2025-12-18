d80ff272ff36e440f10a814c5561c05ee94b6991
Eliud Garcia Ubuntu <jhonniereliudgarciacapera@gmail.com>
2025-12-18 12:10:39 -0500

    generating sums

--- cpp/Dynamic Programming/Generating sums.cpp
+++ cpp/Dynamic Programming/Generating sums.cpp
@@ -38,5 +38,27 @@ int main() {
         }
     }
     cout << ways[x] << ln;
+
+    /* dado un arreglo decir hay un subconjunto
+       tal que la suma sea x
+       para saber la cantidad de formas 
+       es como hacer knapsack
+       solo tamando cada a[i] 1 vez
+    */
+    // O(n * (sum / 64))
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for(auto &i: a) cin >> i;
+
+    const int MAXSUM = int(1e6);
+    bitset<MAXSUM> dp;
+    dp.reset();//todo apagado
+    dp[0] = 1;
+    for(int i = 0; i < n; i++){
+        dp |= (dp << a[i]);
+    }
+
+    cout << dp[x] << ln;
     return 0;
 }
