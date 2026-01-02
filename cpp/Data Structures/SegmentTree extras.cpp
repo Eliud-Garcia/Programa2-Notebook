701c571db0bd215bac01547f213a8afb36eebfb7
Eliud Garcia Ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-01-02 14:52:03 -0500

    delete

--- cpp/Data Structures/SegmentTree extras.cpp
+++ cpp/Data Structures/SegmentTree extras.cpp
@@ -52,3 +52,39 @@ STree<node> st(a);
 
 //para upd
 st.upd(i, node(s[i]));
+
+/*
+para maximum subarray sum
+*/
+
+struct node {
+    ll val, sf, pf, ans;
+    node() : val(0), sf(0), pf(0), ans(0) {}
+    node(ll v) {
+        val = v;
+        sf = max(v, 0LL);
+        pf = max(v, 0LL);
+        ans = max(v, 0LL);
+    }
+};
+T neutro = node();
+
+T oper(T a, T b) {
+    node ans(0);
+    ans.val = a.val + b.val;
+    ans.pf = max(a.pf, a.val + b.pf);
+    ans.sf = max(b.sf, b.val + a.sf);
+    ans.ans = max(a.sf + b.pf, max(a.ans, b.ans));
+    return ans;
+}
+
+vector<int> a(n);
+for(int i = 0; i < n; i++) 
+    cin >> a[i];
+
+vector<node> aux(n);
+for(int i = 0; i < n; i++){
+    aux[i] = node(a[i]);
+}
+
+STree<node> st(aux);
