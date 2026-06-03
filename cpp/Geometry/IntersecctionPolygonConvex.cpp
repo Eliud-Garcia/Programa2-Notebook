1341eda9d65f1be53063774f7c57d821b2f43723
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-06-02 19:56:55 -0500

    convex

--- cpp/Geometry/IntersecctionPolygonConvex.cpp
+++ cpp/Geometry/IntersecctionPolygonConvex.cpp
@@ -1,29 +1,27 @@
-//use struct polygon and point from Polygon.cpp and Point_and_Segments.cpp
 
-bool inside(const Point &a, const Point &b, const Point &p)
-{
+// Traer el struct Point de Objects.cpp
+
+bool inside(const Point& a, const Point& b, const Point& p) {
     return orientation(a, b, p) >= 0;
 }
 
-Point lineIntersection(const Point &a, const Point &b, const Point &c, const Point &d)
-{
+Point lineIntersection(const Point& a, const Point& b, const Point& c,
+                       const Point& d) {
     Point r = b - a, s = d - c;
     double t = (c - a).cross(s) / r.cross(s);
     return a + r * t;
 }
 
-vector<Point> convexIntersection(const vector<Point> &A, const vector<Point> &B)
-{
+vector<Point> convexIntersection(const vector<Point>& A,
+                                 const vector<Point>& B) {
     vector<Point> output = A;
 
-    for (int i = 0; i < (int)B.size(); ++i)
-    {
+    for (int i = 0; i < (int)B.size(); ++i) {
         Point a = B[i], b = B[(i + 1) % B.size()];
         vector<Point> input = output;
         output.clear();
 
-        for (int j = 0; j < (int)input.size(); ++j)
-        {
+        for (int j = 0; j < (int)input.size(); ++j) {
             Point p = input[j], q = input[(j + 1) % input.size()];
             bool pin = inside(a, b, p), qin = inside(a, b, q);
 
@@ -31,8 +29,7 @@ vector<Point> convexIntersection(const vector<Point> &A, const vector<Point> &B)
                 output.push_back(q);
             else if (pin && !qin)
                 output.push_back(lineIntersection(a, b, p, q));
-            else if (!pin && qin)
-            {
+            else if (!pin && qin) {
                 output.push_back(lineIntersection(a, b, p, q));
                 output.push_back(q);
             }
@@ -44,9 +41,7 @@ vector<Point> convexIntersection(const vector<Point> &A, const vector<Point> &B)
     return output;
 }
 
-int main()
-{
-
+int main() {
     vector<Point> A = {{0, 0}, {4, 0}, {4, 4}, {0, 4}};
     vector<Point> B = {{2, -1}, {5, 2}, {2, 5}, {-1, 2}};
     vector<Point> inter = convexIntersection(A, B);
