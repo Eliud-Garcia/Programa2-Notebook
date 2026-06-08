b86dd61a06a93ceb55d1954e0bd4f17ddc8088a4
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-06-08 00:32:39 -0500

    ch

--- cpp/Geometry/Convex Hull.cpp
+++ cpp/Geometry/Convex Hull.cpp
@@ -1,104 +1,108 @@
 
-#define sq(x) ((x)*(ll)(x))
-
-struct pt { // punto
-    ll x, y;
-    pt(ll x_ = 0, ll y_ = 0) : x(x_), y(y_) {}
-    bool operator < (const pt p) const {
-        if (x != p.x) return x < p.x;
-        return y < p.y;
+typedef long long T;
+struct pt {
+    T x, y;
+    pt(T x_ = 0, T y_ = 0) : x(x_), y(y_) {
     }
-    bool operator == (const pt p) const {
-        return x == p.x and y == p.y;
+    bool operator<(const pt p) const {
+        if (x == p.x)
+            return y < p.y;
+        return x < p.x;
     }
-    pt operator + (const pt p) const {
-        return pt(x+p.x, y+p.y);
+    pt operator-(const pt p) const {
+        return pt(x - p.x, y - p.y);
     }
-    pt operator - (const pt p) const {
-        return pt(x-p.x, y-p.y);
+    T operator*(const pt p) const {
+        return x * p.x + y * p.y;
     }
-    pt operator * (const ll c) const {
-        return pt(x*c, y*c);
-    }
-    ll operator * (const pt p) const {
-        return x*(ll)p.x + y*(ll)p.y;
-    }
-    ll operator ^ (const pt p) const {
-        return x*(ll)p.y - y*(ll)p.x;
-    }
-    friend istream& operator >> (istream& in, pt& p) {
-        return in >> p.x >> p.y;
+    T operator^(const pt p) const {
+        return x * p.y - y * p.x;
     }
 };
-
-
-class ConvexHull {
-public:
-    int n;
-    vector<pt> compute(vector<pt> &pts) {
-        n = sz(pts);
-        // sort points by x-axis, and by the y-axis when there's a tie.
-        sort(pts.begin(), pts.end(), [](pt &a, pt &b) {
-            if (b.x < a.x) return false;
-            if (b.x == a.x) return a.y > b.y;
-            return true;
-        });
-        auto low_p = lower_hull(pts);
-        auto upp_p = upper_hull(pts);
-
-        vector<pt> hull;
-        for(auto i: low_p) hull.pb(i);
-        for(auto i: upp_p) hull.pb(i);
-
-        sort(all(hull));
-        hull.erase(unique(all(hull)), hull.end());
-        return hull;
-    }
-
-    vector<pt> lower_hull(vector<pt> &pts) {
-        vector<pt> stk;
-        for (int i = 0; i < n; i++) {
-            while (sz(stk) > 1 && is_cwm(stk[sz(stk) - 2], stk.back(), pts[i]))
-                stk.pop_back();
-            stk.pb(pts[i]);
-        }
-        return stk;
-    }
-    vector<pt> upper_hull(vector<pt> &pts) {
-        vector<pt> stk;
-        for (int i = n - 1; i >= 0; i--) {
-            while (sz(stk) > 1 && is_cwm(stk[sz(stk) - 2], stk.back(), pts[i]))
-                stk.pop_back();
-            stk.pb(pts[i]);
-        }
-        return stk;
-    }
-    //isClockWiseMovement
-    bool is_cwm(pt &a, pt &b, pt &c) {
-        return cross_p(a, b, c) > 0;
-    }
-    //cross product
-    ll cross_p(pt &a, pt &b, pt &c) {
-        return ((a.x - b.x) * (c.y - b.y)) - ((a.y - b.y) * (c.x - b.x));
-    }
-};
-
-int main() {
-    int n;
-    cin >> n;
-    vector<pt> pts(n);
-    forn(i, n) {
-        cin >> pts[i];
-    }
-
-    ConvexHull convexHull;
-    vector<pt > ans = convexHull.compute(pts);
-
-    cout << sz(ans) << ln;
-    for (auto &p: ans)
-        cout << p.x << " " << p.y << ln;
-
+//https://cp-algorithms.com/geometry/convex-hull.html
+int orientation(pt a, pt b, pt c) {
+    T v = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
+    if (v < 0) return -1;  // clockwise
+    if (v > 0) return +1;  // counter-clockwise
     return 0;
 }
 
-//https://cses.fi/problemset/task/2195/
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
+}
+bool ccw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o > 0 || (include_collinear && o == 0);
+}
+
+vector<pt> convex_hull(vector<pt>& a, bool include_collinear = false) {
+    if (sz(a) == 1) return a;
+    sort(all(a));
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down, hull;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < sz(a); i++) {
+        if (i == sz(a) - 1 || cw(p1, a[i], p2, include_collinear)) {
+            while (sz(up) >= 2 && !cw(up[sz(up) - 2], up[sz(up) - 1], a[i], include_collinear))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == sz(a) - 1 || ccw(p1, a[i], p2, include_collinear)) {
+            while (sz(down) >= 2 && !ccw(down[sz(down) - 2], down[sz(down) - 1], a[i], include_collinear))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    if (include_collinear && sz(up) == sz(a)) {
+        reverse(a.begin(), a.end());
+        return a;
+    }
+    for (int i = 0; i < sz(up); i++) hull.push_back(up[i]);
+    for (int i = sz(down) - 2; i > 0; i--) hull.push_back(down[i]);
+    return hull;
+}
+
+// calcula el angulo ABC usando los vectores BA y BC
+// B es el vertice
+double get_angle(pt A, pt B, pt C) {
+    pt BA = B - A, BC = B - C;
+    T dot = BA * BC;
+    T cross = BA ^ BC;
+    return atan2(abs(cross), dot);//angulo en radianes
+}
+
+const double PI = acos(-1.0);
+double RAD_TO_DEG(double rad) {
+    return rad * 180.0 / PI;
+}
+
+int main() {
+    int t; cin >> t;
+    for (int tt = 0; tt < t; tt++) {
+        int n; cin >> n;
+        vector<pt> pts(n);
+        for (int i = 0; i < n; i++) {
+            cin >> pts[i].x >> pts[i].y;
+        }
+        vector<pt> hull = convex_hull(pts);
+        double min_angle = 1e9;
+        if (sz(hull) < 3) {
+            min_angle = 0.0;
+        }else{
+            for (int i = 0; i < sz(hull); i++) {
+                int left = ((i - 1) + sz(hull)) % sz(hull);
+                pt l = hull[left];
+                pt c = hull[i];
+                pt r = hull[(i + 1) % sz(hull)];
+                double ang = get_angle(l, c, r);
+                ang = RAD_TO_DEG(ang);
+                min_angle = min(min_angle, ang);
+            }
+        }
+        printf("Case %d: %.6lf\n", tt + 1, min_angle);
+    }
+    return 0;
+}
